0187cfc47578261e42d0b897555d456a01ee8696
Yudi Kichi <yudi_kichi@hotmail.com>
2018-08-12 20:22:11 -0300

    Final Commit

---
+++ problema1/src/problema1/Problema1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema1;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Yudi
+ */
+public class Problema1 {
+
+    public static ArrayList<String> ListName = new ArrayList();
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void addList(String name){
+        ListName.add(name);
+    }
+    
+    public static void listAll(){
+        
+    }
+}
